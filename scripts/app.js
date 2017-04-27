76dd0460df35335fe9821f9d0e6ae535912d48e1
Daniela Munoz <daniela.munoz@enova.mx>
2017-04-27 17:35:44 -0500

    Adding the fetch event so that it can work offlice

--- scripts/app.js
+++ scripts/app.js
@@ -210,12 +210,11 @@
 
   //
   if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(
-        function(registration){
-          console.log('Service Worker Registered', registration);
-        }
-      );
+    navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function(){
+        console.log('Service Worker Registered');
+    });
   }
 
 })();
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,61 @@
 
 var cacheName = "weatherPWA";
-var filesToCache = [];
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/localforage-1.4.0.js',
+  '/styles/ud811.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
 
-// Event listener que instalará nuestro service worker, que cacheará los recursos requeridos por el  App Shell, html, css, javascript
-self.addEventListener('install', function(e){
+// Install event, will install our service worker which will cache all the elements required by the  App Shell, html, css, javascript
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
   e.waitUntil(
     // open the cache and provide a cache name
-    caches.open(cacheName).then(function(cache){
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
 // Activate event, gets a list of current cache keys and iterates through them using the map function 
-self.addEventListener('activate', function(e){
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keylist){
-      return Promise.all(keyList.map(function(key){
-        // compares the key to the current key and if they're not equal, purges them
-        if (key !== cacheName && key !== dataCacheName ){
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
+    })
+  );
+});
+
+
+
+// Fetch event, to work with slow networks or even offline
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
